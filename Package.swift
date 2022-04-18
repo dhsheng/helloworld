779c7eacc175eb78b855e2f2079f669b64230da3
dhsheng <duanhaisheng@gmail.com>
2022-04-18 14:16:00 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Helloworld",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Helloworld",
+            targets: ["Helloworld"]),
+    ],
+    dependencies: [
+          .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.7.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Helloworld",
+            dependencies: []),
+        .testTarget(
+            name: "HelloworldTests",
+            dependencies: ["Helloworld"]),
+    ]
+)
