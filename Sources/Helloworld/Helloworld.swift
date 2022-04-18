75f654364abca2a0afa9033921db9e605ce8b93d
dhsheng <duanhaisheng@gmail.com>
2022-04-18 14:43:52 +0800

    export symbols

--- Sources/Helloworld/Helloworld.swift
+++ Sources/Helloworld/Helloworld.swift
@@ -1,6 +1,2 @@
-public struct Helloworld {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
+typealias AuthenticateRequest = AuthenticateRequest
+typealias AuthenticateResponse = AuthenticateResponse
