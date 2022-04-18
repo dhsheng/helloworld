607e4f01f61cad706dfabf86909345bf1aa157fa
dhsheng <duanhaisheng@gmail.com>
2022-04-18 14:35:49 +0800

    fix: fix grpc dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Helloworld",
-            dependencies: []),
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift")
+            ]),
         .testTarget(
             name: "HelloworldTests",
             dependencies: ["Helloworld"]),
